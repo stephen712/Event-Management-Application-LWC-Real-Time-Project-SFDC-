4a187387876a8d57822542ce306fc42f2df10fc9
stephen712 <toshashank.singh98@gmail.com>
2023-03-24 17:49:12 +0530

    first commit

---
+++ force-app/main/default/lwc/addEvent/addEvent.js
@@ -0,0 +1,89 @@
+
+import { LightningElement, track } from 'lwc';
+import { createRecord } from 'lightning/uiRecordApi';
+import EVT_OBJECT from '@salesforce/schema/Event__c';
+import Name_F from '@salesforce/schema/Event__c.Name__c';
+import Event_Organizer__c from '@salesforce/schema/Event__c.Event_Organizer__c';
+import Start_DateTime__c from '@salesforce/schema/Event__c.Start_DateTime__c';
+import End_Date_Time__c from '@salesforce/schema/Event__c.End_Date_Time__c';
+import Max_Seats__c from '@salesforce/schema/Event__c.Max_Seats__c';
+import Location__c from '@salesforce/schema/Event__c.Location__c';
+import Event_Detail__c from '@salesforce/schema/Event__c.Event_Detail__c';
+
+import { NavigationMixin } from 'lightning/navigation';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+export default class AddEvent extends NavigationMixin(LightningElement) {
+
+    @track eventRecord = {
+        Name: '',
+        Event_Organizer__c: '',
+        Start_DateTime__c: null,
+        End_Date_Time__c: null,
+        Max_Seats__c: null,
+        Location__c: '',
+        Event_Detail__c: ''
+    }
+    @track errors;
+
+handleChange(event){
+    let value = event.target.value;
+    let name = event.target.name;
+    this.eventRecord[name] = value;
+}
+handleLookup(event){
+    let selectedRecId = event.detail.selectedRecordId;
+    let parentField = event.detail.parentfield;
+    this.eventRecord[parentField] = selectedRecId;
+}
+handleClick(){
+    const fields = {};
+    fields[Name_F.fieldApiName] = this.eventRecord.Name;
+    fields[Event_Organizer__c.fieldApiName] = this.eventRecord.Event_Organizer__c;
+    fields[Start_DateTime__c.fieldApiName] = this.eventRecord.Start_DateTime__c;
+    fields[End_Date_Time__c.fieldApiName] = this.eventRecord.End_Date_Time__c;
+    fields[Max_Seats__c.fieldApiName] = this.eventRecord.Max_Seats__c;
+    fields[Location__c.fieldApiName] = this.eventRecord.Location__c;
+    fields[Event_Detail__c.fieldApiName] = this.eventRecord.Event_Detail__c;
+
+const eventRecord = {apiName: EVT_OBJECT.objectApiName, fields};
+
+createRecord(eventRecord)
+. then((eventRec) => {
+    this.dispatchEvent(new ShowToastEvent({
+        title: 'Record saved',
+        message: 'Event Draft is ready',
+        variant: 'success'
+    }));
+  this[NavigationMixin.Navigate]({
+      type: 'standard__recordPage',
+      attributes: {
+          actionName: "view",
+          recordId: eventRec.id
+      }
+  });
+    //  alert('Record Saved ' + eventRec.id)
+
+}).catch((err) => {
+    this.errors = JSON.stringify(err);
+
+    this.dispatchEvent(new ShowToastEvent({
+        title: 'Error Occured',
+        message: this.errors,
+        variant: 'error'
+    }));
+});
+
+}
+
+handleCancel(){
+    this[NavigationMixin.Navigate]({
+        type: 'standard__objectPage',
+        attributes: {
+            actionName: "home",
+            objectApiName: "Event__c"
+        }
+    });
+}
+
+}
---
+++ force-app/main/default/triggers/EventSpeakerTrigger.trigger
@@ -0,0 +1,52 @@
+trigger EventSpeakerTrigger on EventSpeakers__c (before insert, before update ) {
+    
+  
+    
+    //Step 1 -  Start
+    
+    Set<Id> speakerIdsSet = new Set<Id>();
+    Set<Id> eventIdsSet = new Set<Id>();
+    
+    for( EventSpeakers__c es : Trigger.New ){
+         speakerIdsSet.add(es.Speaker__c);
+         eventIdsSet.add(es.Event__c);
+    }
+    //Step 1 -  End 
+    
+    // Step 2 Start
+    Map<Id, DateTime> requestedEvents = new Map<Id, DateTime>();
+    
+    List<Event__c> relatedEventList = [Select Id, Start_DateTime__c From Event__c 
+                                       Where Id IN : eventIdsSet];
+    
+    for(Event__c evt : relatedEventList ){
+        requestedEvents.put(evt.Id, evt.Start_DateTime__c);
+    }
+    // Step 2 End
+    
+    
+    // Step 3 - Start
+    List<EventSpeakers__c> relatedEventSpeakerList = [ SELECT Id, Event__c, Speaker__c,
+                                               Event__r.Start_DateTime__c
+                                               From EventSpeakers__c
+                                               WHERE Speaker__c IN : speakerIdsSet];
+    
+    // Step 3 - End 
+    
+    // Step 4 - Start
+    for( EventSpeakers__c es : Trigger.New ){ // - Salesforce Geek
+        
+        DateTime bookingTime = requestedEvents.get(es.Event__c); 
+        // DateTime for that event which is associated with this new Event-Speaker Record
+        
+        for(EventSpeakers__c es1 : relatedEventSpeakerList) {
+            if(es1.Speaker__c == es.Speaker__c && es1.Event__r.Start_DateTime__c == bookingTime ){
+                es.Speaker__c.addError('The speaker is already booked at that time');
+                es.addError('The speaker is already booked at that time');
+            }
+        }
+        
+    } 
+    
+    // Step 4 - End
+}
